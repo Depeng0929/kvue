1131dd07e2e955acf76ef1cd5772d81c3725f1dc
depeng.kong <15365182986@163.com>
2020-12-02 21:31:51 +0800

    feat(input): complete input

---
+++ src/components/Input/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <input type="text" :value="current" @input="onInput" />
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+
+export default defineComponent({
+  name: 'AppInput',
+  props: {
+    modelValue: [String, Number],
+  },
+  setup(props, { emit }) {
+    const current = ref(props.modelValue)
+
+    watch(props.modelValue, (val) => {
+      current.value = val
+    })
+
+    function onInput(e) {
+      const value = e.target.value
+      current.value = value
+      emit('update:modelValue', value)
+    }
+    return {
+      current,
+      onInput,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped></style>
