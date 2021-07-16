0176586ff1b0afe9041ee9e52f825b6c8ef6e3bb
depeng.kong <15365182986@163.com>
2021-07-16 17:17:34 +0800

    ci: test

--- packages/virtual/vite.config.ts
+++ packages/virtual/vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'MyLib',
+      name: 'virtual',
+      fileName: 'index',
     },
   },
 })
