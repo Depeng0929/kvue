e6e5233987a9128a284395a02f3d9935754cabc0
depeng.kong <15365182986@163.com>
2021-07-17 21:32:09 +0800

    refactor: change import

---
+++ packages/visible/src/directives.ts
@@ -0,0 +1,42 @@
+import type { DirectiveBinding } from 'vue'
+import ObserveVisibility from './visibleObserve'
+import { deepEqual } from './util'
+
+interface IHTMLElement extends HTMLElement {
+  _vue_visibilityState?: ObserveVisibility
+}
+
+function mounted(el: IHTMLElement, binding: DirectiveBinding) {
+  const { value } = binding
+  el._vue_visibilityState = new ObserveVisibility(el, value)
+}
+
+function updated(el: IHTMLElement, binding: DirectiveBinding) {
+  const { value, oldValue } = binding
+  if (deepEqual(value, oldValue)) return
+
+  if (!value) {
+    unmounted(el)
+    return
+  }
+
+  const state = el._vue_visibilityState
+  if (state)
+    state.createObserver(value)
+  else
+    mounted(el, binding)
+}
+
+function unmounted(el: IHTMLElement) {
+  const state = el._vue_visibilityState
+  if (state) {
+    state.destoryObserver()
+    delete el._vue_visibilityState
+  }
+}
+
+export default {
+  mounted,
+  updated,
+  unmounted,
+}
--- packages/visible/src/index.ts
+++ packages/visible/src/index.ts
@@ -1,48 +1,12 @@
-import type { DirectiveBinding, App } from 'vue'
-import ObserveVisibility from './visibleObserve'
-import { deepEqual } from './util'
+import { App } from 'vue'
+import ObserveDirectives from './directives'
 
-interface IHTMLElement extends HTMLElement {
-  _vue_visibilityState?: ObserveVisibility
+export {
+  ObserveDirectives,
 }
 
-function mounted(el: IHTMLElement, binding: DirectiveBinding) {
-  const { value } = binding
-  el._vue_visibilityState = new ObserveVisibility(el, value)
-}
-
-function updated(el: IHTMLElement, binding: DirectiveBinding) {
-  const { value, oldValue } = binding
-  if (deepEqual(value, oldValue)) return
-
-  if (!value) {
-    unmounted(el)
-    return
-  }
-
-  const state = el._vue_visibilityState
-  if (state)
-    state.createObserver(value)
-  else
-    mounted(el, binding)
-}
-
-function unmounted(el: IHTMLElement) {
-  const state = el._vue_visibilityState
-  if (state) {
-    state.destoryObserver()
-    delete el._vue_visibilityState
-  }
-}
-
-const VObserve = {
+export default {
   install: (app: App) => {
-    app.directive('observe', {
-      mounted,
-      updated,
-      unmounted,
-    })
+    app.directive('observe', ObserveDirectives)
   },
 }
-
-export default VObserve
